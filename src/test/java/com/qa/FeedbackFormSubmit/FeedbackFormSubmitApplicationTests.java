b37938fedd90cffb75b4424c37e3118360c4b329
imallinson <45758809+imallinson@users.noreply.github.com>
2019-02-21 14:30:31 +0000

    paths for docker network

--- src/test/java/com/qa/FeedbackFormSubmit/FeedbackFormSubmitApplicationTests.java
+++ src/test/java/com/qa/FeedbackFormSubmit/FeedbackFormSubmitApplicationTests.java
@@ -15,7 +15,7 @@ public class FeedbackFormSubmitApplicationTests {
     public static void setup() {
         String port = System.getProperty("server.port");
         if (port == null) {
-            RestAssured.port = Integer.valueOf(8085);
+            RestAssured.port = Integer.valueOf(8080);
         }
         else{
             RestAssured.port = Integer.valueOf(port);
@@ -29,7 +29,7 @@ public class FeedbackFormSubmitApplicationTests {
 
         String baseHost = System.getProperty("server.host");
         if(baseHost==null){
-            baseHost = "http://localhost";
+            baseHost = "http://submit";
         }
         RestAssured.baseURI = baseHost;
     }
